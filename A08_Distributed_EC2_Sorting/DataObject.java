d6f635a284270043e0b351ad48e6798baa2a08f0
Harshali Singh <singh.harshali@gmail.com>
2016-09-26 22:22:49 -0400

    removing A08 folder

--- A08_Distributed_EC2_Sorting/DataObject.java
+++
@@ -1,9 +0,0 @@
-
-
-import java.io.Serializable;
-
-@SuppressWarnings("serial")
-public class DataObject implements Serializable{
-	public String dataType;
-	public Object obj;
-}
--- A08_Distributed_EC2_Sorting/IPObject.java
+++
@@ -1,8 +0,0 @@
-
-import java.io.Serializable;
-
-@SuppressWarnings("serial")
-public class IPObject implements Serializable{
-	public String IP;
-	public int serverPort;;
-}
